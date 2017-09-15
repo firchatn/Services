41b6351ab611b38534032e3e80a5a956b292ef51
Firas Chaaben <firaschaabencss@gmail.com>
2017-09-15 11:36:40 +0100

    add model class

--- job/models.py
+++ job/models.py
@@ -3,3 +3,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+# To use it at filter by ville
+class Ville(models.Model):
+    pass
+
+# Service means catecory of servivces : exemples electicite, plamberie ..
+class Service(models.Model):
+    pass
+
+# class form beetwen client and societe mean contra (description du tache)
+class Worker(models.Model):
+    pass
+
+
+
+
